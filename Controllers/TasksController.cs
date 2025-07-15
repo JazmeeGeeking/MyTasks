49c2290c6dea023c0f9f871da2bcadbd7d2aec0c
ST10166517 <ST10166517@vcconnect.edu.za>
2025-07-15 23:20:50 +0200

    Add project files.

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyTasks.Data;
+using MyTasks.Models;
+
+namespace MyTasks.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TasksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.TaskItems.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(TaskItem task)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(task);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(task);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var task = await _context.TaskItems.FindAsync(id);
+            return View(task);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, TaskItem task)
+        {
+            if (id != task.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(task);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(task);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var task = await _context.TaskItems.FindAsync(id);
+            return View(task);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var task = await _context.TaskItems.FindAsync(id);
+            _context.TaskItems.Remove(task);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
